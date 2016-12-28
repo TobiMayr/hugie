ef46309a45a47c4ed8a28bb685868d5671f58267
Tobias Mayr <tobistian@gmail.com>
2016-12-28 00:52:33 -0300

    added toolbar button

--- app/src/main/java/com/example/tobi/hugie/MainActivity.java
+++ app/src/main/java/com/example/tobi/hugie/MainActivity.java
@@ -6,7 +6,10 @@ import android.content.Context;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -55,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
     private String serverIpAddress = "";
 
     private boolean connected = false;
+    private Toolbar toolbar;
 
 
     @Override
@@ -62,7 +66,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listeningOnText = (TextView) findViewById(R.id.listening_on);
-
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
         SERVERIP = getLocalIpAddress();
 
         Thread fst = new Thread(new ServerThread());
@@ -90,6 +95,28 @@ public class MainActivity extends AppCompatActivity {
                 hugImage.setVisibility(View.VISIBLE);
             }
         });*/
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_other) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void CopyIp(){
