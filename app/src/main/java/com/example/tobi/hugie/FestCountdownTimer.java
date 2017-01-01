1ca07db2f56886c37b0b52e34178bdd9742f6fbe
Tobias Mayr <tobistian@gmail.com>
2017-01-01 02:34:58 -0300

    Added ’Days past’ counter, still buggy

---
+++ app/src/main/java/com/example/tobi/hugie/FestCountdownTimer.java
@@ -0,0 +1,24 @@
+package com.example.tobi.hugie;
+
+import android.text.format.Time;
+
+public class FestCountdownTimer {
+    private long intervalMillis;
+    public FestCountdownTimer(int second, int minute, int hour, int monthDay, int month, int year) {
+        Time futureTime = new Time();
+        // Set date to future time
+        futureTime.set(second, minute, hour, monthDay, month, year);
+        futureTime.normalize(true);
+        long futureMillis = futureTime.toMillis(true);
+        Time timeNow = new Time();
+        // Set date to current time
+        timeNow.setToNow();
+        timeNow.normalize(true);
+        long nowMillis = timeNow.toMillis(true);
+        // Subtract current milliseconds time from future milliseconds time to retrieve interval
+        intervalMillis = futureMillis - nowMillis;
+    }
+    public long getIntervalMillis() {
+        return intervalMillis;
+    }
+}
--- app/src/main/java/com/example/tobi/hugie/OtherFragment.java
+++ app/src/main/java/com/example/tobi/hugie/OtherFragment.java
@@ -2,14 +2,18 @@ package com.example.tobi.hugie;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.SystemClock;
 import android.support.annotation.Nullable;
+import android.text.format.Time;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Chronometer;
 import android.widget.TextView;
 
 import java.util.Calendar;
 import java.util.concurrent.TimeUnit;
+import android.os.Handler;
 
 /**
  * Created by tobi on 28.12.16.
@@ -20,6 +24,11 @@ public class OtherFragment extends android.app.Fragment{
     private View otherFragmentView;
     private TextView tvCountdownYear;
     private TextView tvCountdownMonth;
+    //private Chronometer chCountdownSince;
+    TextView tvCountdownSince;
+    private Handler handler;
+    private Runnable runnable;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -27,10 +36,13 @@ public class OtherFragment extends android.app.Fragment{
 
         final TextView tvCountdownYear = (TextView) otherFragmentView.findViewById(R.id.tv_count_year_display);
         final TextView tvCountdownMonth = (TextView) otherFragmentView.findViewById(R.id.tv_count_month_display);
+        final Chronometer chCountdownSince = (Chronometer) otherFragmentView.findViewById(R.id.ch_count_since_display);
+        tvCountdownSince = (TextView) otherFragmentView.findViewById(R.id.tv_count_since_display);
         int year = 2015;
         int month = 1;
         int day = 7;
 
+        countDownStart();
         final long ALMOST_YEAR= 1000*60*60*24*300;
         final long ALMOST_MONTH= 1000*60*60*24*27;
         Calendar endCalendar = Calendar.getInstance();
@@ -40,6 +52,32 @@ public class OtherFragment extends android.app.Fragment{
         long endMillis = endCalendar.getTimeInMillis(); //get the end time in milliseconds
         long totalMillis = (endMillis - startMillis); //total time in milliseconds
 
+/*
+        chCountdownSince.setBase(endMillis);
+
+        chCountdownSince.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+            public void onChronometerTick(Chronometer cArg) {
+                long t = System.currentTimeMillis() - chCountdownSince.getBase();
+
+                long days = TimeUnit.MILLISECONDS.toDays(t);
+                t -= TimeUnit.DAYS.toMillis(days);
+
+                long hours = TimeUnit.MILLISECONDS.toHours(t);
+                t -= TimeUnit.HOURS.toMillis(hours);
+
+                long minutes = TimeUnit.MILLISECONDS.toMinutes(t);
+                t -= TimeUnit.MINUTES.toMillis(minutes);
+
+                long seconds = TimeUnit.MILLISECONDS.toSeconds(t);
+                final String leftYearString = String.format(getString(R.string.left_year_string), days, hours, minutes, seconds);
+                tvCountdownSince.post(new Runnable() {
+                    public void run() {
+                        tvCountdownSince.setText(leftYearString);
+                    }
+                });
+            }
+        });
+*/
         for (long i = totalMillis; i < 0; i+=ALMOST_YEAR) {
             if (totalMillis < 0){
                 endCalendar.set(++year, month, day);
@@ -120,4 +158,38 @@ public class OtherFragment extends android.app.Fragment{
 
         return otherFragmentView;
     }
+
+    public void countDownStart() {
+        handler = new Handler();
+        runnable = new Runnable(){
+            @Override
+            public void run(){
+                handler.postDelayed(this, 1000);
+                try {
+                    FestCountdownTimer timer = new FestCountdownTimer(00, 00, 9, 3, 01, 2011);
+                    new CountDownTimer(-timer.getIntervalMillis(), 1000) {
+                        @Override
+                        public void onTick(long millisUntilFinished){
+                            int days = (int) ((millisUntilFinished / 1000) / 86400);
+                            int hours = (int) (((millisUntilFinished / 1000)
+                                    - (days * 86400)) / 3600);
+                            int minutes = (int) (((millisUntilFinished / 1000)
+                                    - (days * 86400) - (hours * 3600)) / 60);
+                            int seconds = (int) ((millisUntilFinished / 1000) % 60);
+                            String daysPast = String.format("Days past: %d", days);
+                            tvCountdownSince.setText(daysPast);
+                        }
+                        @Override
+                        public void onFinish() {
+
+                        }
+                    }.start();
+                } catch (Exception e) {
+                    e.printStackTrace();                }
+            }
+        };
+        handler.postDelayed(runnable, 1 * 1000);
+
+    }
 }
+
