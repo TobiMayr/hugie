362e124ec1fd19e63185ede01d2a30db3288213c
Tobias Mayr <tobistian@gmail.com>
2017-01-10 02:42:06 -0300

    added datePicker

--- app/src/main/java/com/example/tobi/hugie/OtherFragment.java
+++ app/src/main/java/com/example/tobi/hugie/OtherFragment.java
@@ -3,16 +3,16 @@ package com.example.tobi.hugie;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
 import java.util.Calendar;
 import java.util.concurrent.TimeUnit;
-import android.os.Handler;
 
 /**
  * Created by tobi on 28.12.16.
@@ -22,9 +22,6 @@ public class OtherFragment extends android.app.Fragment{
 
     private View otherFragmentView;
     private Chronometer chCountdownSince;
-    int year = 2015;
-    int month = 1;
-    int day = 7;
     final long ALMOST_YEAR= 1000*60*60*24*300;
     final long ALMOST_MONTH= 1000*60*60*24*27;
     private TextView daysAni;
@@ -39,6 +36,23 @@ public class OtherFragment extends android.app.Fragment{
     private TextView hoursSince;
     private TextView minutesSince;
     private TextView secondsSince;
+    private int year;
+    private int month;
+    private int day;
+    Calendar endCalendar = Calendar.getInstance();
+    long startMillis;
+    long endMillis;
+    long totalMillis;
+    long totalMillisMonth;
+    long totalMillisYear;
+    CountDownTimer cdtm;
+    CountDownTimer cdt;
+
+
+    DatePicker picker;
+    Button displayDate;
+    TextView currentDateLbl;
+    boolean calendarOpen = false;
 
     @Nullable
     @Override
@@ -58,17 +72,24 @@ public class OtherFragment extends android.app.Fragment{
         hoursSince = (TextView) otherFragmentView.findViewById(R.id.hours_since);
         minutesSince = (TextView) otherFragmentView.findViewById(R.id.minutes_since);
         secondsSince = (TextView) otherFragmentView.findViewById(R.id.seconds_since);
+
         year = 2015;
         month = 1;
         day = 7;
-
-        Calendar endCalendar = Calendar.getInstance();
         endCalendar.set(year, month, day, 0, 0, 0);
 
-        long startMillis = System.currentTimeMillis(); //get the start time in milliseconds
-        long endMillis = endCalendar.getTimeInMillis(); //get the end time in milliseconds
-        long totalMillis = (endMillis - startMillis); //total time in milliseconds
+        startMillis = System.currentTimeMillis(); //get the start time in milliseconds
+        endMillis = endCalendar.getTimeInMillis(); //get the end time in milliseconds
+        totalMillis = (endMillis - startMillis); //total time in milliseconds
+        totalMillisYear = getMissingMilliesAni();
+        totalMillisMonth = getMissingMilliesMens();
 
+        currentDateLbl =(TextView) otherFragmentView.findViewById(R.id.lblDate);
+        picker=(DatePicker)otherFragmentView.findViewById(R.id.dpResult);
+        displayDate=(Button)otherFragmentView.findViewById(R.id.btnChangeDate);
+
+        picker.updateDate(year, month, day);
+        currentDateLbl.setText(getCurrentDate());
         chCountdownSince.setBase(endMillis);
 
         chCountdownSince.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
@@ -93,36 +114,8 @@ public class OtherFragment extends android.app.Fragment{
         });
         chCountdownSince.start();
 
-        for (long i = totalMillis; i < 0; i+=ALMOST_YEAR) {
-            if (totalMillis < 0){
-                endCalendar.set(++year, month, day);
-                endMillis = endCalendar.getTimeInMillis();
-                totalMillis = (endMillis - startMillis);
-            }
-            else
-                break;
-        }
 
-        long endMillisMonth = endMillis;
-        long totalMillisMonth = totalMillis;
-
-        for (long i = totalMillisMonth; i < 0 || i > ALMOST_MONTH; i-=ALMOST_MONTH) {
-            if (totalMillisMonth > ALMOST_MONTH){
-                endCalendar.set(year, --month, day);
-                endMillisMonth = endCalendar.getTimeInMillis();
-                totalMillisMonth = (endMillisMonth - startMillis);
-            }
-            else
-                break;
-            if (totalMillisMonth < 0){
-                endCalendar.set(year, ++month, day);
-                endMillisMonth = endCalendar.getTimeInMillis();
-                totalMillisMonth = (endMillisMonth - startMillis);
-                break;
-            }
-        }
-
-        CountDownTimer cdt = new CountDownTimer(totalMillis, 1000) {
+        cdt = new CountDownTimer(totalMillisYear, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
                 long days = TimeUnit.MILLISECONDS.toDays(millisUntilFinished);
@@ -147,7 +140,7 @@ public class OtherFragment extends android.app.Fragment{
         };
         cdt.start();
 
-        CountDownTimer cdtm = new CountDownTimer(totalMillisMonth, 1000) {
+        cdtm = new CountDownTimer(totalMillisMonth, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
                 long days = TimeUnit.MILLISECONDS.toDays(millisUntilFinished);
@@ -172,7 +165,86 @@ public class OtherFragment extends android.app.Fragment{
         };
         cdtm.start();
 
+        displayDate.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                if (calendarOpen){
+                    picker.setVisibility(View.GONE);
+                    currentDateLbl.setText(getCurrentDate());
+                    endCalendar.set(year, month, day, 0, 0, 0);
+                    endMillis = endCalendar.getTimeInMillis();
+                    totalMillis = (endMillis - startMillis);
+                    chCountdownSince.setBase(endMillis);
+                    totalMillisYear = getMissingMilliesAni();
+                    totalMillisMonth = getMissingMilliesMens();
+                    cdt.start();
+                    cdtm.start();
+                    calendarOpen = false;
+                }
+                else {
+                    picker.setVisibility(View.VISIBLE);
+                    calendarOpen = true;
+                }
+            }
+        });
+
         return otherFragmentView;
     }
+
+    public String getCurrentDate(){
+        StringBuilder builder = new StringBuilder();
+        builder.append("Current Date: ");
+        builder.append(picker.getDayOfMonth() + "/");
+        builder.append((picker.getMonth() + 1) + "/");//month is 0 based
+        builder.append(picker.getYear());
+        day = picker.getDayOfMonth();
+        month = picker.getMonth();
+        year = picker.getYear();
+        return builder.toString();
+    }
+
+    public long getMissingMilliesMens(){
+        long endMillisMonth;
+        long totalMillisTemp = totalMillis;
+        int monthTemp = month;
+        Calendar endCalendarTemp = Calendar.getInstance();
+
+        for (long i = totalMillisTemp; i < 0 || i > ALMOST_MONTH; i-=ALMOST_MONTH) {
+            if (totalMillisTemp > ALMOST_MONTH){
+                endCalendarTemp.set(year, --monthTemp, day);
+                endMillisMonth = endCalendarTemp.getTimeInMillis();
+                totalMillisTemp = (endMillisMonth - startMillis);
+            }
+            else
+                break;
+            if (totalMillisTemp < 0){
+                endCalendarTemp.set(year, ++monthTemp, day);
+                endMillisMonth = endCalendarTemp.getTimeInMillis();
+                totalMillisTemp = (endMillisMonth - startMillis);
+                break;
+            }
+        }
+        return  totalMillisTemp;
+    }
+
+    public long getMissingMilliesAni(){
+        long totalMillisTemp = totalMillis;
+        Calendar endCalendarTemp = Calendar.getInstance();
+        int yearTemp = year;
+
+        for (long i = totalMillisTemp; i < 0; i+=ALMOST_YEAR) {
+            if (totalMillisTemp < 0){
+                endCalendarTemp.set(++yearTemp, month, day);
+                long endMillisTemp = endCalendarTemp.getTimeInMillis();
+                totalMillisTemp = (endMillisTemp - startMillis);
+            }
+            else
+                break;
+        }
+        return totalMillisTemp;
+    }
 }
+
+
+
 
